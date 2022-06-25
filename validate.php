b5ac9a5bcd5cd2948a87a56a54554f363477932b
gursewak1311 <106023022+gursewak1311@users.noreply.github.com>
2022-06-25 11:24:36 -0400

    Create validate.php

---
+++ validate.php
@@ -0,0 +1,31 @@
+<?php
+//validate our user information, checking to see whether information has been entered and whether it is in the proper format
+
+//creating an empty array to store error messages 
+$errors = array();
+//check to see whether data has been entered and whether it is the correct format, provide a heplful error message if not and set the flag variable to true 
+
+if (empty($input_title)) {
+  $error_msg_1 = "Please enter Title of your blog";
+  array_push($errors, $error_msg_1);
+}
+
+if (empty($input_date) || $input_date === false) {
+  $error_msg_2 = "Please enter the date";
+  array_push($errors, $error_msg_2);
+}
+if (empty($input_body)) {
+  $error_msg_3 = "Please enter Body of your blog";
+  array_push($errors, $error_msg_3);
+}
+if (empty($input_category)) {
+  $error_msg_4 = "Please enter Category of your blog";
+  array_push($errors, $error_msg_4);
+}
+if ($responseData["success"] === false) {
+  $error_msg_5 = "No robots please!!";
+  array_push($errors, $error_msg_5);
+}
+
+
+?>
