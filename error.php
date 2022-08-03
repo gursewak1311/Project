3dc438ad330b4cb476bfacf12fb0bddb5ad4a6f4
gursewak1311 <106023022+gursewak1311@users.noreply.github.com>
2022-08-03 19:28:49 -0400

    Create error.php

---
+++ error.php
@@ -0,0 +1,13 @@
+<!-- this is our custom error page -->
+<?php require_once('header.php'); ?>
+  <div class="container">
+    <header>
+      <h1> Blogosphere problem </h1>
+      <h2> Sorry :( </h2>
+    </header>
+    <main>
+        <p class="text-center"> Something went wrong! An email has been sent to our team to let us know! </p> 
+        <a href="index.php" class="text-center"> Head back to our homepage? </a>
+    </main>
+    <!--require global footer here -->
+    <?php require_once('footer.php'); ?>
