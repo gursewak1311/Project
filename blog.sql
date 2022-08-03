1028a8dcbae127ef5089091eaf1a6859bc22bed3
gursewak1311 <106023022+gursewak1311@users.noreply.github.com>
2022-08-03 19:53:48 -0400

    Update blog.sql

--- blog.sql
+++ blog.sql
@@ -6,3 +6,12 @@ CREATE TABLE `blogs` (
   `category` varchar (100) NOT NULL,
   PRIMARY KEY (user_id)
 );
+CREATE TABLE `users` (
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar (100) NOT NULL,
+  `email` varchar (100) NOT NULL,
+  `username` varchar (100) NOT NULL,
+  `password` varchar (255) NOT NULL,
+  PRIMARY KEY (user_id)
+);
