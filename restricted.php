52e4982e795b01ea4500a27f12b8e69225186278
gursewak1311 <106023022+gursewak1311@users.noreply.github.com>
2022-08-04 01:48:31 -0400

    Create restricted.php

---
+++ restricted.php
@@ -0,0 +1,19 @@
+<?php require_once('header.php'); ?>
+  <div class="container">
+    <header>
+      <h1> Blogs </h1>
+     
+    </header>
+    <main class="view">
+      <div class="row">
+       
+        <div class="col-md-8">
+          <h3> Sorry, only registered users can view blogs </h3>
+         <a href="register.php" class="ts_button">  Want to Join Us? </a>
+         <a href="login.php" class="ts_button">  Need to Login? </a>
+        </div>
+      </div>
+      <!--end row-->
+    </main>
+    <!--require global footer here -->
+    <?php require_once('footer.php'); ?>
