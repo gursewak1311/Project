627c1decfe202f76ecd6479619b882f2cc9460ec
gursewak1311 <106023022+gursewak1311@users.noreply.github.com>
2022-08-03 21:54:11 -0400

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+// access the existing session
+session_start();
+
+// remove all session variables
+session_unset();
+
+// destroy the user session
+session_destroy();
+
+// redirect to login
+header('location:index.php');
+
+?>
