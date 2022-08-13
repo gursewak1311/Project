3dad186be4ec50e0be7b66d34549b02d3e5e8f65
gursewak1311 <106023022+gursewak1311@users.noreply.github.com>
2022-08-13 13:12:00 -0400

    Update view.php

--- view.php
+++ view.php
@@ -1,23 +1,5 @@
 <?php require_once('header.php'); ?>
-<body class="home">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="home.php">Blog</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="view.php">View Blogs</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+<?php require_once('navigation.php'); ?>
   <div class="container">
      <header>
        <h1> Blog Site </h1>
