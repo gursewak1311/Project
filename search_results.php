5a42396929c5deff306d5b68b69bc0edbe90f0bd
gursewak1311 <106023022+gursewak1311@users.noreply.github.com>
2022-08-14 04:02:17 -0400

    Update search_results.php

--- search_results.php
+++ search_results.php
@@ -2,22 +2,18 @@
 <?php require_once 'header.php'; ?>
 <div class="container">
     <?php
-   
+
     $search = filter_input(INPUT_GET, 'keywords');
     echo "<p class='alert alert-info'> You searched for the following: " . $search . "</p>";
 
-    //let's use explode to break a search phrase into keywords 
-
     $search_words = explode(' ', $search);
-
-    //we use the explode function to break apart our search phrase. The first parameter is the boundary (i.e. where we should split it up - in this case it would be spaces between the words )
-
-    //let's build the start of our query 
+  
     $sql = "SELECT * FROM blogs WHERE ";
 
+    //making a string that will contain the desired sql command
     $where = "";
     foreach ($search_words as $word) {
-        $where = $where . "title LIKE '%$word%' OR "; //we continue to build the query 
+        $where = $where . "title LIKE '%$word%' OR body LIKE '%$word%' OR category LIKE '%$word%' OR "; //we continue to build the query
     }
 
     $where = substr($where, 0, strlen($where) - 4);
@@ -29,9 +25,9 @@
     $statement = $db->prepare($final_query);
     //execute
     $statement->execute();
-    //use fetchAll to access the rows in the data, store in $results 
+    //use fetchAll to access the rows in the data, store in $results
     $results = $statement->fetchAll();
-    
+
     echo "<table class='table table-striped'>";
     foreach ($results as $result) {
         echo "<tr>";
@@ -44,9 +40,10 @@
     //close the table element
     echo "</table>";
 
-    //close cursor 
+    //close cursor
     $statement->closeCursor();
 
     ?>
 </div><!-- close .container-->
 <?php require_once 'footer.php'; ?>
+
